130a1bf0cbceeeb06266e49253421baa41a6a261
AveWycc220 <mik16748@gmail.com>
2020-09-11 15:16:17 +0300

    feat (SPAT/fourier): add args '-show', '-first', '-second', '-third'

--- fourier/src/plots.py
+++ fourier/src/plots.py
@@ -2,7 +2,7 @@ import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt, mpld3
 import pandas as pd
-import os
+import os, sys
 import math
 
 " CONST "
@@ -10,7 +10,7 @@ DATA_DIR = os.path.join( os.path.dirname( __file__ ), '..', 'data\\' )
 A = -4
 B = 4
 STEP = 0.1
-N = 10000
+N = 1000
 
 " Functions "
 first_default = lambda x: x
@@ -41,39 +41,68 @@ lambda_third_fourier = np.vectorize(third_fourier)
 sns.set_theme()
 " Created values "
 values = np.arange(A, B, STEP, dtype=float)
-" First plot "
-default = pd.DataFrame({'x':values,
-                       'f':first_default(values)})
-fourier = pd.DataFrame({'x':values,
-                       'f':lambda_first_fourier(values)})
-plt.figure()
-fig, ax = plt.subplots()
-first_plot = sns.lineplot(data=default, x="x", y="f", linewidth = 2)
-second_plot = sns.lineplot(data=fourier, x="x", y="f", linewidth = 2)
-plt.legend(labels=['Default', 'Fourier'])
-plt.savefig(f"{DATA_DIR}\\plot1.png")
-mpld3.save_html(fig, f"{DATA_DIR}\\plot1.html")
-" Second plot "
-default = pd.DataFrame({'x':values,
-                       'f':second_default(values)})
-fourier = pd.DataFrame({'x':values,
-                       'f':lambda_second_fourier(values)})
-plt.figure()
-fig, ax = plt.subplots()
-first_plot = sns.lineplot(data=default, x="x", y="f", linewidth = 2)
-second_plot = sns.lineplot(data=fourier, x="x", y="f", linewidth = 2)
-plt.legend(labels=['Default', 'Fourier'])
-plt.savefig(f"{DATA_DIR}\\plot2.png")
-mpld3.save_html(fig, f"{DATA_DIR}\\plot2.html")
-" Third plot "
-default = pd.DataFrame({'x':values,
-                       'f':third_default(values)})
-fourier = pd.DataFrame({'x':values,
-                       'f':lambda_third_fourier(values)})
-plt.figure()
-fig, ax = plt.subplots()
-first_plot = sns.lineplot(data=default, x="x", y="f", linewidth = 2)
-second_plot = sns.lineplot(data=fourier, x="x", y="f", linewidth = 2)
-plt.legend(labels=['Default', 'Fourier'])
-plt.savefig(f"{DATA_DIR}\\plot3.png")
-mpld3.save_html(fig, f"{DATA_DIR}\\plot3.html")
+
+def first_plot(): 
+    " First plot "
+    default = pd.DataFrame({'x':values,
+                        'f':first_default(values)})
+    fourier = pd.DataFrame({'x':values,
+                        'f':lambda_first_fourier(values)})
+    plt.figure()
+    if ('-show' not in sys.argv): 
+        fig = plt.subplots()
+    sns.lineplot(data=default, x="x", y="f", linewidth = 2)
+    sns.lineplot(data=fourier, x="x", y="f", linewidth = 2)
+    plt.legend(labels=['Default', 'Fourier'])
+    if ('-show' not in sys.argv): 
+        plt.savefig(f"{DATA_DIR}\\plot1.png")
+        mpld3.save_html(fig[0], f"{DATA_DIR}\\plot1.html")
+    else: 
+        plt.show()
+def second_plot():
+    " Second plot "
+    default = pd.DataFrame({'x':values,
+                        'f':second_default(values)})
+    fourier = pd.DataFrame({'x':values,
+                        'f':lambda_second_fourier(values)})
+    plt.figure()
+    if ('-show' not in sys.argv): 
+        fig = plt.subplots()
+    sns.lineplot(data=default, x="x", y="f", linewidth = 2)
+    sns.lineplot(data=fourier, x="x", y="f", linewidth = 2)
+    plt.legend(labels=['Default', 'Fourier'])
+    if ('-show' not in sys.argv): 
+        plt.savefig(f"{DATA_DIR}\\plot2.png")
+        mpld3.save_html(fig[0], f"{DATA_DIR}\\plot2.html")
+    else: 
+        plt.show()
+
+def third_plot():
+    " Third plot "
+    default = pd.DataFrame({'x':values,
+                        'f':third_default(values)})
+    fourier = pd.DataFrame({'x':values,
+                        'f':lambda_third_fourier(values)})
+    plt.figure()
+    if ('-show' not in sys.argv): 
+        fig = plt.subplots()
+    sns.lineplot(data=default, x="x", y="f", linewidth = 2)
+    sns.lineplot(data=fourier, x="x", y="f", linewidth = 2)
+    plt.legend(labels=['Default', 'Fourier'])
+    if ('-show' not in sys.argv): 
+        plt.savefig(f"{DATA_DIR}\\plot3.png")
+        mpld3.save_html(fig[0], f"{DATA_DIR}\\plot3.html")
+    else: 
+        plt.show()
+
+if __name__ == "__main__":
+    if "-first" in sys.argv:
+        first_plot()
+    elif "-second" in sys.argv:
+        second_plot()
+    elif "-third" in sys.argv: 
+        third_plot()
+    else:
+        first_plot()
+        second_plot()
+        third_plot()
